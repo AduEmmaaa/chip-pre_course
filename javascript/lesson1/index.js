5107724090d81314c5ae172f6cd6b167fbcae532
AduEmmaaa <Adu956641@gmail,com>
2023-06-28 15:09:01 +0000

    javascript/lesson1/index.js

---
+++ javascript/lesson1/index.js
@@ -0,0 +1,20 @@
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    start: function(){
+        console.log("engine has started")
+    },
+    stop: function(){
+        console.log("engine is about stoping")
+    }
+  };
+ // console.log(person.stop);
+//console.log(
+    person.gender="male";
+    person.age=56;
+
+    person.start();  
+    person.stop();
+    console.log(person.gender);
+    console.log(person.age);
